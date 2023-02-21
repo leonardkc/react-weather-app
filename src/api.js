27f0812120e59b8427dd0e9f487e071cdb9bfdb8
leonardkc <ndulueleonard@gmail.com>
2023-02-21 22:53:23 +0100

    Completed weather app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a1e74358ccmsha38d4d6eb48cc37p15d570jsn96c942a4818e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b1619b48213eb27fac3c901bb7ab6728";
